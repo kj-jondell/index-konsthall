9ea14c126ecdf72f684df5b94ba45d5c4d722b58
Karl Johannes <kj@jondell.com>
2022-05-12 09:23:36 +0200

    some minor updates

--- buttons.py
+++ buttons.py
@@ -9,18 +9,23 @@ import RPi.GPIO as GPIO
 from pythonosc import udp_client
 
 BUTTONS = [18, 16, 38, 40] # GPIO PINS ON RPI
+LEDS = [11, 13, 35, 37] # GPIO PINS ON RPI
 SC_PORT = 57120 #use command line args instead?
 try:
     SC_PORT = os.environ["SC_LISTEN_PORT"]
 except KeyError:
     pass #Use default port if sc port not set
 
+channelStates = [1, 1, 1, 1] # states for the channel
+
 client = udp_client.SimpleUDPClient("127.0.0.1", SC_PORT)
 
 def button_pressed_callback(channel):
     for ind, val in enumerate(BUTTONS):
         if channel == val:
-            client.send_message("/channel", ind)
+            channelStates[ind] = (channelStates[ind]+1)%2
+            client.send_message("/channel", channelStates)
+            GPIO.output(LEDS[ind], GPIO.HIGH if channelStates[ind] == 1 else GPIO.LOW)
     #print(f"Button {channel} pressed!")
 
 def signal_handler(sig, frame):
@@ -31,6 +36,8 @@ GPIO.setmode(GPIO.BOARD)
 for buttonPin in BUTTONS:
     GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(buttonPin, GPIO.FALLING, callback=button_pressed_callback, bouncetime=250) 
+for ledPin in LEDS:
+    GPIO.setup(ledPin, GPIO.OUT)
 
 signal.signal(signal.SIGINT, signal_handler)
 signal.pause()
